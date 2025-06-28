12175e955421ba055255b605a8a14d818bfa06e8
Tirth Patel <109569104+ptirth2184@users.noreply.github.com>
2025-06-28 05:31:59 +0000

    Added CSV/Excel uploader

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import pandas as pd 
 
 st.title('FinWise: Smart Expense Categorizer & Budget Planner')
-st.write('Welcome to your personal finance aassistant!')
+
+st.header('step1: Upload your bank statement')
+uploaded_file = st.file_uploader('upload your CSV or Excel file', type=['csv', 'excel'])
+
+if uploaded_file:
+    if uploaded_file.name.endswith('.csv'):
+        df = pd.read_csv(uploaded_file)
+    else:
+        df = pd.read_excel(uploaded_file)
+
+    st.success('File uploaded succesfully')
+    st.subheader('🔍 Preview of Transactions')
+    st.dataframe(df.head(10))
+
+else:
+    st.info('please upload a bank statement to continue')
